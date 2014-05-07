fcab99bfd3f1401d65ca175970787cc5724de5d0
Johannes <johannesgotlen@Hello-There-Chip.local>
2014-05-07 09:36:40 +0200

    new licence http://www.apache.org/licenses/LICENSE-2.0

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,24 +1,16 @@
-﻿//
-//  Tests.cs
+﻿//  Copyright 2014 Johannes Gotlen <johannes@gotlen.se>
 //
-//  Author:
-//       Johannes Gotlen <johannesgotlen.se>
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
 //
-//  Copyright (c) 2014 
+//        http://www.apache.org/licenses/LICENSE-2.0
 //
-//  This library is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as
-//  published by the Free Software Foundation; either version 2.1 of the
-//  License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful, but
-//  WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
 
 using NUnit.Framework;
 using System;
@@ -77,7 +69,7 @@ namespace JohJSON
 			string json = n.ToString();
 			Console.WriteLine(json);
 			Tokenizer w = new Tokenizer();
-			var result = w.ParseText(new StringReader(json));
+			w.ParseText(new StringReader(json));
 			//foreach (var s in result)
 			//	Console.WriteLine(s);
 			Generator g = new Generator();
@@ -232,11 +224,10 @@ namespace JohJSON
 		[Test()]
 		public void CorrectCommaSeparationException()
 		{
-			JSONNode n;
 			bool exeptionThrown = false;
 			try{
-				n = JSONNode.CreateFromString("{ \"field\" : false, \"second field\": true \"third field\":false }");
-			}catch(Exception e){
+				JSONNode.CreateFromString("{ \"field\" : false, \"second field\": true \"third field\":false }");
+			}catch{
 				exeptionThrown = true;
 			}
 			Assert.IsTrue(exeptionThrown);
